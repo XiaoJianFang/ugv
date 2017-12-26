57db302d0a99dd0389ce346e7753572f73de434d
Lemoxit <yangshuai93@hotmail.com>
2017-12-26 16:47:20 +0800

    add packetV2X.h

---
+++ packetV2X.h
@@ -0,0 +1,27 @@
+//
+// Created by lemoxit on 17-12-15.
+//
+
+#ifndef DOMINANT_PACKETV2X_H
+#define DOMINANT_PACKETV2X_H
+
+#include "./V2XCommItf/CauseCodeType.hpp"
+#include "./V2XCommItf/CollisionRisk_SubCauseCodeType.hpp"
+#include "./V2XCommItf/EventPosition.hpp"
+#include "./V2XCommItf/EventType.hpp"
+#include "./V2XCommItf/HazardousLocation_ObstacleOnTheRoad_SubCauseCodeType.hpp"
+#include "./V2XCommItf/OtherVehicleData.hpp"
+#include "./V2XCommItf/PacketIncomingEventData.hpp"
+#include "./V2XCommItf/PacketIncomingPlatooningReq.hpp"
+#include "./V2XCommItf/PacketIncomingPlatooningRslt.hpp"
+#include "./V2XCommItf/PacketIncomingPlatooningRsp.hpp"
+#include "./V2XCommItf/PacketOtherVehicleData_list.hpp"
+#include "./V2XCommItf/PacketPlatooningInfo.hpp"
+#include "./V2XCommItf/PacketPlatooningReq.hpp"
+#include "./V2XCommItf/PacketPlatooningRsltNotify.hpp"
+#include "./V2XCommItf/PacketPlatooningRsp.hpp"
+#include "./V2XCommItf/PlatooningAct.hpp"
+#include "./V2XCommItf/PlatooningRslt.hpp"
+#include "./V2XCommItf/PlatooningRspCode.hpp"
+
+#endif //DOMINANT_PACKETV2X_H
