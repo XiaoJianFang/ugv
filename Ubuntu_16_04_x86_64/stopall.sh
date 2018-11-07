eab0ea72f22af6a063ea2b506575d5131f74c5dc
KangBinTu <35772731+KangBinTu@users.noreply.github.com>
2018-11-07 17:41:17 +0800

    update

--- Ubuntu_16_04_x86_64/stopall.sh
+++ Ubuntu_16_04_x86_64/stopall.sh
@@ -2,7 +2,7 @@
 
 ps -ef | grep Control | grep -v grep | cut -c 9-15 | xargs kill -s 9
 
-ps -ef | grep Fusion2017 | grep -v grep | cut -c 9-15 | xargs kill -s 9
+ps -ef | grep Fusion2018 | grep -v grep | cut -c 9-15 | xargs kill -s 9
 
 ps -ef | grep Planner | grep -v grep | cut -c 9-15 | xargs kill -s 9
 
@@ -16,6 +16,9 @@ ps -ef | grep LidarDriverVector | grep -v grep | cut -c 9-15 | xargs kill -s 9
 
 ps -ef | grep RSDS_MBE | grep -v grep | cut -c 9-15 | xargs kill -s 9
  # ps -ef | grep VSLAM | grep -v grep | cut -c 9-15 | xargs kill -s 9
+ps -ef | grep Vehicle_Simulator | grep -v grep | cut -c 9-15 | xargs kill -s 9
+
+ps -ef |grep bash| grep moduleWatchDog.sh | grep -v grep | cut -c 9-15 | xargs kill -s 9
 
 
 
